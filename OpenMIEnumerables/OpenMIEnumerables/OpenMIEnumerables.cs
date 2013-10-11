b0172060d1664cc8635f122466c77eaec18216bc
Tim Eicher <tim@timeicher.com>
2013-10-10 21:26:00 -0500

    Initial project checkin. Includes a few extension methods and need dependencies.

---
+++ OpenMIEnumerables/OpenMIEnumerables/OpenMIEnumerables.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using OpenMI.Standard;
+
+namespace OpenMI.Extensions
+{
+    public static class Enumerables
+    {
+        public static IEnumerable<IOutputExchangeItem> GetOutputExchangeItems(this ILinkableComponent component)
+        {
+            for (int i = 0; i < component.OutputExchangeItemCount; i++)
+            {
+                yield return component.GetOutputExchangeItem(i);
+            }
+        }
+
+        public static IEnumerable<IInputExchangeItem> GetInputExchangeItems(this ILinkableComponent component)
+        {
+            for (int i = 0; i < component.InputExchangeItemCount; i++)
+            {
+                yield return component.GetInputExchangeItem(i);
+            }
+        }
+
+        public static IEnumerable<IDataOperation> GetDataOperations(this IOutputExchangeItem item)
+        {
+            for (int i = 0; i < item.DataOperationCount; i++)
+            {
+                yield return item.GetDataOperation(i);
+            }
+        }
+    }
+}
